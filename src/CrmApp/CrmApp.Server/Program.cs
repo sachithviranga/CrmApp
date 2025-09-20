0e9bcc5761a43858ae5d4bab8752c4d110a048f6
Sac <sachith.viranga@gmail.com>
2025-09-21 02:52:17 +0800

    added ResponseCompression

--- src/CrmApp/CrmApp.Server/Program.cs
+++ src/CrmApp/CrmApp.Server/Program.cs
@@ -1,9 +1,46 @@
 using CrmApp.Server.Extensions;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Register Services
-ServiceCollectionExtension.RegisterServices(builder.Services , builder.Configuration);
+ServiceCollectionExtension.RegisterServices(builder.Services, builder.Configuration);
+
+#region ResponseCompression
+
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+
+    options.Providers.Add<GzipCompressionProvider>();
+
+    options.Providers.Add<BrotliCompressionProvider>();
+
+    options.MimeTypes =
+            [
+        // Default
+        "text/plain",
+                    "text/css",
+                    "application/javascript",
+                    "text/html",
+                    "application/xml",
+                    "text/xml",
+                    "application/json",
+                    "text/json",
+                    // Custom
+                    "image/svg+xml",
+                     "application/atom+xml"
+    ];
+
+    builder.Services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+});
+
+builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+{
+    options.Level = System.IO.Compression.CompressionLevel.Optimal;
+});
+
+#endregion
 
 // Add services to the container.
 
