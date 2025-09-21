eec9dc5dd617a7bd9671645d727b3c14b72bcc5c
Sac <sachith.viranga@gmail.com>
2025-09-21 23:46:04 +0800

    imlemented sweet alert

---
+++ src/CrmApp/CrmApp.Client/wwwroot/js/alerts.js
@@ -0,0 +1,22 @@
+﻿window.showAlert = (type, title, message) => {
+    Swal.fire({
+        icon: type,
+        title: title,
+        text: message,
+        showConfirmButton: false,
+        timer: 3000
+    });
+};
+
+window.showConfirm = (title, text) => {
+    return Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'Cancel'
+    }).then(result => result.isConfirmed);
+};
